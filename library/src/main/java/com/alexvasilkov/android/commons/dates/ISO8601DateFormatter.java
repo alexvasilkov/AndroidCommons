8422d3187d49ff0234e2ed830ce50d98f710528f
Alex Vasilkov <alexvasilkov@gmail.com>
2017-05-21 16:57:03 +0700

    Removed AdvancedDateFormatter. Clean up in ISO8601DateFormatter.

--- library/src/main/java/com/alexvasilkov/android/commons/dates/ISO8601DateFormatter.java
+++ library/src/main/java/com/alexvasilkov/android/commons/dates/ISO8601DateFormatter.java
@@ -3,6 +3,7 @@ package com.alexvasilkov.android.commons.dates;
 import java.text.ParseException;
 import java.util.Date;
 
+@SuppressWarnings("unused") // Public API
 public class ISO8601DateFormatter {
 
     private static final ThreadSafeDateFormatter ISO8601_PARSER =
@@ -13,34 +14,58 @@ public class ISO8601DateFormatter {
 
 
     /**
-     * Produces string in format <code>yyyy-MM-ddTHH:mm:ss+00:00</code>
+     * Produces string in format <code>yyyy-MM-dd'T'HH:mm:ss'Z'</code>.
      */
     public static String formatISO8601(Date date) {
-        return ISO8601_FORMATTER.format(date) + "+00:00";
+        return ISO8601_FORMATTER.format(date) + "Z";
     }
 
     /**
      * Parses dates in following formats:
      * <pre>
      *   yyyy-MM-dd
-     *   yyyy-MM-ddTHH:mm:ss[.S+]
-     *   yyyy-MM-ddTHH:mm:ss[.S+]Z
-     *   yyyy-MM-ddTHH:mm:ss[.S+]±hh[[:]mm]
+     *   yyyy-MM-dd'T'HH:mm:ss[.S+]
+     *   yyyy-MM-dd'T'HH:mm:ss[.S+]Z
+     *   yyyy-MM-dd'T'HH:mm:ss[.S+]±hh[[:]mm]
+     * </pre>
+     *
+     * Examples:
+     * <pre>
+     *   1988-03-06
+     *   1988-03-06T12:33:14
+     *   1988-03-06T12:33:14.12345
+     *   1988-03-06T12:33:14Z
+     *   1988-03-06T12:33:14,123Z
+     *   1988-03-06T12:33:14+03
+     *   1988-03-06T12:33:14.123+03
+     *   1988-03-06T12:33:14-04:30
+     *   1988-03-06T12:33:14-0430
+     *   1988-03-06T12:33:14,1-04:30
      * </pre>
      */
     public static Date parseISO8601(String str) throws ParseException {
-        if (str == null) throw new NullPointerException("String to parse is null");
+        if (str == null) {
+            throw new NullPointerException("String to parse is null");
+        }
 
-        if (str.charAt(str.length() - 1) == 'Z') str = str.substring(0, str.length() - 1) + "+0000";
+        if (str.charAt(str.length() - 1) == 'Z') {
+            str = str.substring(0, str.length() - 1) + "+0000";
+        }
 
         // Stripping milliseconds
         int dotIndex = str.indexOf('.');
-        if (dotIndex == -1) dotIndex = str.indexOf(',');
+        if (dotIndex == -1) {
+            dotIndex = str.indexOf(',');
+        }
 
         if (dotIndex != -1) {
             int endIndex = str.indexOf('+', dotIndex);
-            if (endIndex == -1) endIndex = str.indexOf('-', dotIndex);
-            if (endIndex == -1) endIndex = str.length();
+            if (endIndex == -1) {
+                endIndex = str.indexOf('-', dotIndex);
+            }
+            if (endIndex == -1) {
+                endIndex = str.length();
+            }
             str = str.substring(0, dotIndex) + str.substring(endIndex);
         }
 
@@ -53,41 +78,13 @@ public class ISO8601DateFormatter {
             str += "00";
         }
 
-        if (str.charAt(str.length() - 3) == ':')
+        if (str.charAt(str.length() - 3) == ':') {
             str = str.substring(0, str.length() - 3) + str.substring(str.length() - 2);
+        }
 
-        // At this point we should have string in format yyyy-MM-ddTHH:mm:ss±hhmm
+        // At this point we should have string in format yyyy-MM-dd'T'HH:mm:ss±hhmm
 
         return ISO8601_PARSER.parse(str);
     }
-
-//    public static void testParser() {
-//        String[] tests = {
-//                "1988-03-06",
-//                "1988-03-06T12:33:14",
-//                "1988-03-06T12:33:14.12345",
-//                "1988-03-06T12:33:14Z",
-//                "1988-03-06T12:33:14,123Z",
-//                "1988-03-06T12:33:14+03",
-//                "1988-03-06T12:33:14.123+03",
-//                "1988-03-06T12:33:14-04:30",
-//                "1988-03-06T12:33:14-0430",
-//                "1988-03-06T12:33:14,1-04:30"
-//        };
-//
-//        for (String str : tests) {
-//            test(str);
-//        }
-//    }
-//
-//    private static void test(String str) {
-//        try {
-//            Date parsed = parseISO8601(str);
-//            String back = formatISO8601(parsed);
-//            Log.d("DATETEST", str + " -> " + back + " // " + parsed.toString());
-//        } catch (Exception e) {
-//            Log.e("DATETEST", "Can't parse date: " + str + " // " + e.getMessage());
-//        }
-//    }
 
 }
